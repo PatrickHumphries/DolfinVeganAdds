fdf7036a4b6381bb779ef18bd7192aedff999c38
patrick humphries <patrickhumphries95@gmail.com>
2024-01-21 02:12:14 -0500

    initial commit

---
+++ Contents/mods/DolfinVeganFood/media/lua/server/Items/DolfinVeganAdds_Distro.lua
@@ -0,0 +1,49 @@
+require "Items/ProceduralDistributions"
+
+--Tofu--
+
+        table.insert(ProceduralDistributions["list"]["BreakRoomShelves"].items, "Base.Tofu");
+        table.insert(ProceduralDistributions["list"]["BreakRoomShelves"].items, 1);
+
+        table.insert(ProceduralDistributions["list"]["CafeteriaSnacks"].items, "Base.Tofu");
+        table.insert(ProceduralDistributions["list"]["CafeteriaSnacks"].items, 1);
+
+        table.insert(ProceduralDistributions["list"]["ChineseKitchenFridge"].items, "Base.Tofu");
+        table.insert(ProceduralDistributions["list"]["ChineseKitchenFridge"].items, 1);
+
+        table.insert(ProceduralDistributions["list"]["FreezerGeneric"].items, "Base.Tofu");
+        table.insert(ProceduralDistributions["list"]["FreezerGeneric"].items, 1);
+
+        table.insert(ProceduralDistributions["list"]["KitchenBreakfast"].items, "Base.Tofu");
+        table.insert(ProceduralDistributions["list"]["KitchenBreakfast"].items, 1);
+
+        table.insert(ProceduralDistributions["list"]["StoreShelfCombo"].items, "Base.Tofu");
+        table.insert(ProceduralDistributions["list"]["StoreShelfCombo"].items, 1);
+
+        table.insert(ProceduralDistributions["list"]["SushiKitchenFridge"].items, "Base.Tofu");
+        table.insert(ProceduralDistributions["list"]["SushiKitchenFridge"].items, 1);
+
+--Tempeh--
+        
+        table.insert(ProceduralDistributions["list"]["BreakRoomShelves"].items, "Base.VTempeh");
+        table.insert(ProceduralDistributions["list"]["BreakRoomShelves"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["CafeteriaSnacks"].items, "Base.VTempeh");
+        table.insert(ProceduralDistributions["list"]["CafeteriaSnacks"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["ChineseKitchenFridge"].items, "Base.VTempeh");
+        table.insert(ProceduralDistributions["list"]["ChineseKitchenFridge"].items, 1);
+
+        table.insert(ProceduralDistributions["list"]["FreezerGeneric"].items, "Base.Tempeh");
+        table.insert(ProceduralDistributions["list"]["FreezerGeneric"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["KitchenBreakfast"].items, "Base.VTempeh");
+        table.insert(ProceduralDistributions["list"]["KitchenBreakfast"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["StoreShelfCombo"].items, "Base.VTempeh");
+        table.insert(ProceduralDistributions["list"]["StoreShelfCombo"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["SushiKitchenFridge"].items, "Base.VTempeh");
+        table.insert(ProceduralDistributions["list"]["SushiKitchenFridge"].items, 1);
+
+
