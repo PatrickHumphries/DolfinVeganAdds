2388645d79186fae6e03274678bb69dfa21eee82
patrick humphries <patrickhumphries95@gmail.com>
2024-01-21 14:40:11 -0500

    WIP add plant milks, buff fried tofu

--- Contents/mods/DolfinVeganFood/media/lua/server/Items/DolfinVeganAdds_Distro.lua
+++ Contents/mods/DolfinVeganFood/media/lua/server/Items/DolfinVeganAdds_Distro.lua
@@ -1,6 +1,6 @@
 require "Items/ProceduralDistributions"
 
---Tofu--
+--Tofu
 
         table.insert(ProceduralDistributions["list"]["BreakRoomShelves"].items, "Base.Tofu");
         table.insert(ProceduralDistributions["list"]["BreakRoomShelves"].items, 1);
@@ -23,7 +23,30 @@ require "Items/ProceduralDistributions"
         table.insert(ProceduralDistributions["list"]["SushiKitchenFridge"].items, "Base.Tofu");
         table.insert(ProceduralDistributions["list"]["SushiKitchenFridge"].items, 1);
 
---Tempeh--
+--Fried Tofu
+
+        table.insert(ProceduralDistributions["list"]["BreakRoomShelves"].items, "Base.TofuFried");
+        table.insert(ProceduralDistributions["list"]["BreakRoomShelves"].items, 1);
+
+        table.insert(ProceduralDistributions["list"]["CafeteriaSnacks"].items, "Base.TofuFried");
+        table.insert(ProceduralDistributions["list"]["CafeteriaSnacks"].items, 1);
+
+        table.insert(ProceduralDistributions["list"]["ChineseKitchenFridge"].items, "Base.TofuFried");
+        table.insert(ProceduralDistributions["list"]["ChineseKitchenFridge"].items, 1);
+
+        table.insert(ProceduralDistributions["list"]["FreezerGeneric"].items, "Base.TofuFried");
+        table.insert(ProceduralDistributions["list"]["FreezerGeneric"].items, 1);
+
+        table.insert(ProceduralDistributions["list"]["KitchenBreakfast"].items, "Base.TofuFried");
+        table.insert(ProceduralDistributions["list"]["KitchenBreakfast"].items, 1);
+
+        table.insert(ProceduralDistributions["list"]["StoreShelfCombo"].items, "Base.TofuFried");
+        table.insert(ProceduralDistributions["list"]["StoreShelfCombo"].items, 1);
+
+        table.insert(ProceduralDistributions["list"]["SushiKitchenFridge"].items, "Base.TofuFried");
+        table.insert(ProceduralDistributions["list"]["SushiKitchenFridge"].items, 1);
+
+--Tempeh
         
         table.insert(ProceduralDistributions["list"]["BreakRoomShelves"].items, "Base.VTempeh");
         table.insert(ProceduralDistributions["list"]["BreakRoomShelves"].items, 1);
@@ -45,5 +68,129 @@ require "Items/ProceduralDistributions"
         
         table.insert(ProceduralDistributions["list"]["SushiKitchenFridge"].items, "Base.VTempeh");
         table.insert(ProceduralDistributions["list"]["SushiKitchenFridge"].items, 1);
+
+--Oat Milk
+
+        table.insert(ProceduralDistributions["list"]["BakeryKitchenFridge"].items, "Base.VOatMilk");
+        table.insert(ProceduralDistributions["list"]["BakeryKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["CafeKitchenFridge"].items, "Base.VOatMilk");
+        table.insert(ProceduralDistributions["list"]["CafeKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["CrepeKitchenFridge"].items, "Base.VOatMilk");
+        table.insert(ProceduralDistributions["list"]["CrepeKitchenFridge"].items, 1);
+
+        table.insert(ProceduralDistributions["list"]["DeepFryKitchenFridge"].items, "Base.VOatMilk");
+        table.insert(ProceduralDistributions["list"]["DeepFryKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["DinerKitchenFridge"].items, "Base.VOatMilk");
+        table.insert(ProceduralDistributions["list"]["DinerKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["FishChipsKitchenFridge"].items, "Base.VOatMilk");
+        table.insert(ProceduralDistributions["list"]["FishChipsKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, "Base.VOatMilk");
+        table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["FridgeOther"].items, "Base.VOatMilk");
+        table.insert(ProceduralDistributions["list"]["FridgeOther"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["FridgeRich"].items, "Base.VOatMilk");
+        table.insert(ProceduralDistributions["list"]["FridgeRich"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, "Base.VOatMilk");
+        table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["ItalianKitchenFridge"].items, "Base.VOatMilk");
+        table.insert(ProceduralDistributions["list"]["ItalianKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["JaysKitchenFridge"].items, "Base.VOatMilk");
+        table.insert(ProceduralDistributions["list"]["JaysKitchenFridge"].items, 1);
+      
+        table.insert(ProceduralDistributions["list"]["RestaurantKitchenFridge"].items, "Base.VOatMilk");
+        table.insert(ProceduralDistributions["list"]["RestaurantKitchenFridge"].items, 1);
+
+--Soy Milk
+
+        table.insert(ProceduralDistributions["list"]["BakeryKitchenFridge"].items, "Base.VSoyMilk");
+        table.insert(ProceduralDistributions["list"]["BakeryKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["CafeKitchenFridge"].items, "Base.VSoyMilk");
+        table.insert(ProceduralDistributions["list"]["CafeKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["CrepeKitchenFridge"].items, "Base.VSoyMilk");
+        table.insert(ProceduralDistributions["list"]["CrepeKitchenFridge"].items, 1);
+
+        table.insert(ProceduralDistributions["list"]["DeepFryKitchenFridge"].items, "Base.VSoyMilk");
+        table.insert(ProceduralDistributions["list"]["DeepFryKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["DinerKitchenFridge"].items, "Base.VSoyMilk");
+        table.insert(ProceduralDistributions["list"]["DinerKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["FishChipsKitchenFridge"].items, "Base.VSoyMilk");
+        table.insert(ProceduralDistributions["list"]["FishChipsKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, "Base.VSoyMilk");
+        table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["FridgeOther"].items, "Base.VSoyMilk");
+        table.insert(ProceduralDistributions["list"]["FridgeOther"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["FridgeRich"].items, "Base.VSoyMilk");
+        table.insert(ProceduralDistributions["list"]["FridgeRich"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, "Base.VSoyMilk");
+        table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["ItalianKitchenFridge"].items, "Base.VSoyMilk");
+        table.insert(ProceduralDistributions["list"]["ItalianKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["JaysKitchenFridge"].items, "Base.VSoyMilk");
+        table.insert(ProceduralDistributions["list"]["JaysKitchenFridge"].items, 1);
+      
+        table.insert(ProceduralDistributions["list"]["RestaurantKitchenFridge"].items, "Base.VSoyMilk");
+        table.insert(ProceduralDistributions["list"]["RestaurantKitchenFridge"].items, 1);
+
+--Almond Milk
+
+        table.insert(ProceduralDistributions["list"]["BakeryKitchenFridge"].items, "Base.VAlmondMilk");
+        table.insert(ProceduralDistributions["list"]["BakeryKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["CafeKitchenFridge"].items, "Base.VAlmondMilk");
+        table.insert(ProceduralDistributions["list"]["CafeKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["CrepeKitchenFridge"].items, "Base.VAlmondMilk");
+        table.insert(ProceduralDistributions["list"]["CrepeKitchenFridge"].items, 1);
+
+        table.insert(ProceduralDistributions["list"]["DeepFryKitchenFridge"].items, "Base.VAlmondMilk");
+        table.insert(ProceduralDistributions["list"]["DeepFryKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["DinerKitchenFridge"].items, "Base.VAlmondMilk");
+        table.insert(ProceduralDistributions["list"]["DinerKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["FishChipsKitchenFridge"].items, "Base.VAlmondMilk");
+        table.insert(ProceduralDistributions["list"]["FishChipsKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, "Base.VAlmondMilk");
+        table.insert(ProceduralDistributions["list"]["FridgeGeneric"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["FridgeOther"].items, "Base.VAlmondMilk");
+        table.insert(ProceduralDistributions["list"]["FridgeOther"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["FridgeRich"].items, "Base.VAlmondMilk");
+        table.insert(ProceduralDistributions["list"]["FridgeRich"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, "Base.VAlmondMilk");
+        table.insert(ProceduralDistributions["list"]["FridgeTrailerPark"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["ItalianKitchenFridge"].items, "Base.VAlmondMilk");
+        table.insert(ProceduralDistributions["list"]["ItalianKitchenFridge"].items, 1);
+        
+        table.insert(ProceduralDistributions["list"]["JaysKitchenFridge"].items, "Base.VAlmondMilk");
+        table.insert(ProceduralDistributions["list"]["JaysKitchenFridge"].items, 1);
+      
+        table.insert(ProceduralDistributions["list"]["RestaurantKitchenFridge"].items, "Base.VAlmondMilk");
+        table.insert(ProceduralDistributions["list"]["RestaurantKitchenFridge"].items, 1);
+
 
 
